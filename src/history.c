ac788269982d5c868295fdd1e52012e1c88816fc
Daniel Holguin <doholguin@miners.utep.edu>
2022-09-21 19:34:04 -0600

    updating from Lap

--- src/history.c
+++ src/history.c
@@ -1,1 +1,48 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include "history.h"
+#include "tokenizer.h"
+
+List* init_history(){
+  List *list = malloc(sizeof(List));
+  list->root = malloc(sizeof(Item));
+  return list;
+}
+void add_history(List *list, char *str){
+  Item *h = (Item*) malloc(sizeof(Item));
+  int id = 0;
+  h->str = str;
+
+  if(list->id == 0){
+    h->next = NULL;
+    list->root = h;
+  }
+  else{
+    h->next = list->root;
+    list ->root = h;
+  }
+  list -> id++;
+}
+char *get_history(List *list, int id){
+  Item *temp= list -> root;// temporary list
+  while( temp != NULL){
+    if( temp -> id == id) {
+      return temp -> str;
+    }
+    temp = temp -> next;
+  }
+  puts("No History found");
+  return NULL;
+}
+void print_history(List *list){
+  if(!list){
+    puts("There is no log Histroy!");
+  }
+  else{
+    Item *temp = list-> root;
+    while(temp != NULL){
+      printf("user input [%d]: %s \n" temp->id ,get_history(list,temp->id));
+      temp = temp-> next;
+    }
+  }
+}
--- src/uimain.c
+++ src/uimain.c
@@ -7,23 +7,39 @@ int main()
 {
   while(1){
     char choice [10];
-    char sentence [100];
+    char str [100];
+    List *history = init_history();
     printf("please choose the an option\n a) Tokenizer\n b) History\n q) quit \n>");
     fgets(choice,sizeof(choice),stdin);
+    add_history(history, choice);
     switch(choice[0]){
     case 'a':
       printf("You have choosen the Tokenizer option \n please enter a sentecne or word: \n>");
-      fgets(sentence, sizeof(sentence),stdin); //getting the sentence or words.
-      printf("1:true 0:false [%d]\n",space_char(sentence[0])); //getting the sentence to the Token object.
-      printf("1:true 0:false [%d]\n",non_space_char(sentence[0]));
-      puts(word_start(sentence));
-      puts(word_terminator(sentence));
-      printf("The total amount of words are: [%d]\n",count_words(sentence));
+      fgets(str, sizeof(str),stdin); //getting the sentence or words.
+      /* debbuging <- phases delete or remove this part and the last (* /)
+      printf("1:true 0:false [%d]\n",space_char(str[0])); //getting the sentence to the Token object.
+      printf("1:true 0:false [%d]\n",non_space_char(str[0]));
+      puts(word_start(str));
+      puts(word_terminator(str));
+      printf("The total amount of words are: [%d]\n",count_words(str));
+      char *stest = word_start(str);
+      char *etest = word_terminator(str);
+      char *test = copy_str(str, etest - stest);
+      puts(test);
+      */
+      add_history(history,str);
+      char **tokens = tokenize(str);
+      print_tokens(tokens);
+      free_tokens(tokens);
       break;
     case 'b':
+      print_history(history);
       break;
     case 'q':
       goto done;
+    default:
+      puts("Not a choice try again!!");
+      break;
     }
   }
  done:
